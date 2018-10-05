354079c851815c11e219d066cddcb44c80d3e45a
saninstein <saninstein@gmail.com>
2018-10-05 16:47:16 +0300

    Update CustomAlgorithm.py

--- strategy/sma_cross/CustomAlgorithm.py
+++ strategy/sma_cross/CustomAlgorithm.py
@@ -70,7 +70,7 @@ class BasicTemplateAlgorithm(QCAlgorithm):
 
         self.SetCash(10000)  # Set Strategy Cash
 
-        res = Resolution.Monthly
+        res = Resolution.Daily
 
         self.SetBrokerageModel(BrokerageName.GDAX, AccountType.Cash)
 
